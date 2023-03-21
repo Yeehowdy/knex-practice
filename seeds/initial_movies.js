b672e925d368dc209445e5f88b594d19f279c233
Yeehowdy <kylemersinger67@gmail.com>
2023-03-21 16:31:36 +0100

    seed stuff

--- seeds/initial_movies.js
+++ seeds/initial_movies.js
@@ -1,13 +1,13 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
- */
-exports.seed = async function(knex) {
+// seeds/initial_movies.js
+exports.seed = function(knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
-  ]);
+  return knex('movies').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('movies').insert([
+        {id: 1, title: 'Vicky Cristina Barcelona', genre: 'drama', release_date: '2008-08-15'},
+        {id: 2, title: 'Orfeu Negro', genre: 'drama', release_date: '1959-12-21'},
+        {id: 3, title: 'Midnight in Paris', genre: 'drama', release_date: '2011-05-20'},
+      ]);
+    });
 };
